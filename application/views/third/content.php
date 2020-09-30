21dbd391574c0121bd1fa7fd2ab1dbb3c6499ca7
conelo03 <ramadhan071thea@gmail.com>
2020-09-30 21:12:31 +0700

    a
    a

--- application/views/third/content.php
+++ application/views/third/content.php
@@ -4,32 +4,131 @@
 	=============================================-->
 
 	<div class="cosmetics-home-intro-area mb-70">
-		<div class="container">
+		<div class="container" style="background-color: ;">
+			<div style="background-color: #D2B48C; position: absolute; width: 400px; height: 700px; margin-bottom: 50px; margin-top: 75px;">
+				
+			</div>
+
 			<div class="row align-items-center">
-				<div class="col-md-6 mb-30">
-					<div class="cosmetics-home-intro">
-						<p class="label">NEW COLLECTION /</p>
-						<h2 class="title">Professional <br> skin care for you</h2>
-						<p class="description">Lorem ipsum dolor sit amet, consectetur cing elit. Suspe ndisse suscipit sagittis leo
-							sit met condimentum estibulum issim Lorem ipsum dolor sit amet, consectetur cing elit.</p>
-						<a href="shop-left-sidebar.html" class="lezada-shop-link">LEARN MORE</a>
+				
 
-					</div>
-				</div>
-
-				<div class="col-md-6 pl-60 pl-sm-15 pl-xs-15 pl-xxs-15 mb-30">
+				<div class="col-md-6 pl-60 pl-sm-15 pl-xs-15 pl-xxs-15 mb-30 mt-30" style="background-color: ;">
+					
 					<!--=======  single image  =======-->
-
-					<div class="single-banner-image text-center text-md-right">
-						<img src="assets/images/banners/img-comestic-1.jpg" class="img-fluid" alt="">
+					<div class="row text-center">
+						<div class="col-md-6 mb-30">					
+							<img src="<?php base_url();?>assets/images/mf/gold series/gold.png" class="img-fluid" alt="">
+						</div>
+						<div class="col-md-6 mb-30">
+							<img src="<?php base_url();?>assets/images/mf/gold series/caviar.png" class="img-fluid" alt="">
+						</div>
 					</div>
-
+					<div class="single-product">
+						<div class="single-product__image">
+							<a class="image-wrap center" href="shop-product-basic.html">
+			                    <img src="assets/images/mf/gold series/after.png" class="img-fluid" alt="">
+			                    <img src="assets/images/mf/gold series/before.png" class="img-fluid" alt="">
+			                    
+			                 </a>
+							
+						</div>
+					</div>
 					<!--=======  End of single image  =======-->
+				</div>
+				<div class="col-md-6 mb-30">
+					<div class="cosmetics-home-intro text-center">
+						<h3 class="title text-center">GOLD SERIES</h3>
+						<img src="assets/images/mf/prod/gol series-10.png" class="img-fluid" alt="">
+						<a href="shop-left-sidebar.html" class="lezada-shop-link text-center">LEARN MORE</a>
+
+					</div>
 				</div>
 			</div>
 		</div>
 	</div>
 
+	<div class="cosmetics-home-intro-area mb-70">
+		<div class="container" style="background-color: ;">
+			<div class="row align-items-center">
+				<div style="background-color: #BA55D3; position: absolute; width: 300px; height: 300px; margin-left: 770px; margin-bottom: 50px; margin-top: 50px;">
+				
+				</div>
+
+				<div class="col-md-6 mb-30">
+					<div class="cosmetics-home-intro text-center">
+						<h3 class="title text-center">EYE PRIORITY</h3>
+						<img src="assets/images/mf/prod/eye priority-10.png" class="img-fluid" alt="">
+						<a href="shop-left-sidebar.html" class="lezada-shop-link text-center">LEARN MORE</a>
+
+					</div>
+				</div>
+				<div class="col-md-6 pl-60 pl-sm-15 pl-xs-15 pl-xxs-15 mb-30 mt-30">
+					<!--=======  single image  =======-->
+					<div class="single-product">
+					<!--=======  single image  =======-->
+						
+						<div class="single-product__image">
+							<a class="image-wrap center" href="shop-product-basic.html">
+			                    <img src="assets/images/mf/eye priority/before.png" class="img-fluid" alt="">
+			                    <img src="assets/images/mf/eye priority/after.png" class="img-fluid" alt="">
+			                    
+			                 </a>
+							
+						</div>
+					</div>
+					<div class="row text-center">
+						<div class="col-md-6 ml-10 mb-30">					
+							<img src="<?php base_url();?>assets/images/mf/eye priority/cryo.png" class="img-fluid" alt="">
+						</div>
+					</div>
+					
+					<!--=======  End of single image  =======-->
+				</div>
+				
+			</div>
+		</div>
+	</div>
+
+	<div class="cosmetics-home-intro-area mb-70">
+		<div class="container" style="background-color: ;">
+			<div class="row align-items-center">
+				<div style="background-color: #FFB6C1; position: absolute; width: 500px; height: 400px; margin-left: 65px; margin-bottom: 50px; margin-top: 150px;">
+				
+				</div>
+
+				<div class="col-md-6 pl-60 pl-sm-15 pl-xs-15 pl-xxs-15 mb-30 mt-30">
+					
+					<!--=======  single image  =======-->
+					<div class="row text-center">
+						<div class="col-md-3 mb-30 text-center"></div>
+						<div class="col-md-6 mb-30 text-center">					
+							<img src="assets/images/mf/lips xl/aset-26.png" class="img-fluid" alt="">
+						</div>
+					</div>
+					<div class="single-product">
+						<div class="single-product__image">
+							<a class="image-wrap center" href="shop-product-basic.html">
+			                    
+			                    <img src="<?php base_url();?>assets/images/mf/lips xl/aset-01.png" class="img-fluid" alt="">
+			                    <img src="<?php base_url();?>assets/images/mf/lips xl/aset-27.png" class="img-fluid" alt="">
+			                    
+			                </a>
+							
+						</div>
+					</div>
+					<!--=======  End of single image  =======-->
+				</div>
+				<div class="col-md-6 mb-30">
+					<div class="cosmetics-home-intro text-center">
+						<h3 class="title text-center">LIPS XL</h3>
+						<img src="assets/images/mf/prod/gol series-10.png" class="img-fluid" alt="">
+						<a href="shop-left-sidebar.html" class="lezada-shop-link text-center">LEARN MORE</a>
+
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 	<!--=====  End of cosmetics home intro section  ======-->
 
 	<!--=============================================
--- application/views/third/prod.php
+++ application/views/third/prod.php
@@ -4,36 +4,25 @@
 	=============================================-->
 
   <div class="product-category-container mb-90 mb-md-85 mb-sm-85">
-    <div class="container wide">
+    <div class="container">
       <div class="row">
-        <div class="col-lg-12">
           <!--=======  product category wrapper  =======-->
 
-          <div class="lezada-slick-slider product-category-slider wow fadeInUp" data-slick-setting='{
-						"slidesToShow": 3,
-						"arrows": true,
-						"autoplay": true,
-						"autoplaySpeed": 5000,
-						"speed": 1000,
-						"prevArrow": {"buttonClass": "slick-prev", "iconClass": "ion-ios-arrow-thin-left" },
-						"nextArrow": {"buttonClass": "slick-next", "iconClass": "ion-ios-arrow-thin-right" }
-					}' data-slick-responsive='[
-						{"breakpoint":1501, "settings": {"slidesToShow": 3} },
-						{"breakpoint":1199, "settings": {"slidesToShow": 3} },
-						{"breakpoint":991, "settings": {"slidesToShow": 2, "arrows": false} },
-						{"breakpoint":767, "settings": {"slidesToShow": 1, "arrows": false} },
-						{"breakpoint":575, "settings": {"slidesToShow": 1, "arrows": false} },
-						{"breakpoint":479, "settings": {"slidesToShow": 1, "arrows": false} }
-					]'>
-
-            <div class="col">
+          
+            <div class="col-lg-1"></div>
+            <div class="col-lg-2">
               <!--=======  single category  =======-->
 
-              <div class="single-category single-category--two">
+              <div class="single-product">
                 <!--=======  single category image  =======-->
 
-                <div class="single-category__image single-category__image--two">
-                  <img src="<?php base_url();?>assets/images/mf/prod/pd_1.png" class="img-fluid" alt="">
+                <div class="single-product__image">
+                  <a class="image-wrap center" href="shop-product-basic.html">
+                    <img src="<?php base_url();?>assets/images/mf/prod/pd_1.png" class="img-fluid" alt="">
+                    <img src="<?php base_url();?>assets/images/mf/prod/pd_3.png" class="img-fluid" alt="">
+                    
+                  </a>
+                  
                 </div>
 
                 <!--=======  End of single category image  =======-->
@@ -60,15 +49,20 @@
 
               <!--=======  End of single category  =======-->
             </div>
-
-            <div class="col">
+            <div class="col-lg-2"></div>
+             <div class="col-lg-2">
               <!--=======  single category  =======-->
 
-              <div class="single-category single-category--two">
+              <div class="single-product">
                 <!--=======  single category image  =======-->
 
-                <div class="single-category__image single-category__image--two">
-                  <img src="<?php base_url();?>assets/images/mf/prod/pd_2.png" class="img-fluid" alt="">
+                <div class="single-product__image">
+                  <a class="image-wrap center" href="shop-product-basic.html">
+                    <img src="<?php base_url();?>assets/images/mf/prod/pd_2_.png" class="img-fluid" alt="">
+                    <img src="<?php base_url();?>assets/images/mf/prod/pd_2_2.png" class="img-fluid" style="" alt="">
+                    
+                  </a>
+                  
                 </div>
 
                 <!--=======  End of single category image  =======-->
@@ -95,15 +89,20 @@
 
               <!--=======  End of single category  =======-->
             </div>
-
-            <div class="col">
+            <div class="col-lg-2"></div>
+             <div class="col-lg-2">
               <!--=======  single category  =======-->
 
-              <div class="single-category single-category--two">
+              <div class="single-product">
                 <!--=======  single category image  =======-->
 
-                <div class="single-category__image single-category__image--two">
-                  <img src="<?php base_url();?>assets/images/mf/prod/pd_3.png" class="img-fluid" alt="">
+                <div class="single-product__image">
+                  <a class="image-wrap center" href="shop-product-basic.html">
+                    <img src="<?php base_url();?>assets/images/mf/prod/pd_3.png" class="img-fluid" alt="">
+                    <img src="<?php base_url();?>assets/images/mf/prod/pd_1.png" class="img-fluid" alt="">
+                    
+                  </a>
+                  
                 </div>
 
                 <!--=======  End of single category image  =======-->
@@ -130,11 +129,10 @@
 
               <!--=======  End of single category  =======-->
             </div>
-
-          </div>
+            <div class="col-lg-1"></div>
+          
 
           <!--=======  End of product category wrapper  =======-->
-        </div>
       </div>
     </div>
   </div>
