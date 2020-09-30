79ddf9fb42995794eb6e0aa3cecb7245bf8a6e79
conelo03 <ramadhan071thea@gmail.com>
2020-09-30 23:24:15 +0700

    a
    a

--- application/views/third/content-eye.php
+++ application/views/third/content-eye.php
@@ -33,8 +33,138 @@
 					<img src="assets/images/mf/prod/pd_2_.png" class="img-fluid" alt="">
 					<!--=======  End of single image  =======-->
 				</div>
-				<div class="col-md-6 mb-30">
-					<div class="cosmetics-home-intro">
+				<div class="col-md-6 mb-30"  style="background-color: #dccca7; height: 400px;">
+					<div class="cosmetics-home-intro mt-50">
+						<div class="tp-caption   tp-resizeme" id="slide-14-layer-9" data-x="['left','left','left','left']"
+							data-hoffset="['287','93','89','21']" data-y="['top','top','top','top']"
+							data-voffset="['170','73','58','108']" data-width="none" data-height="none" data-whitespace="nowrap"
+							data-type="text" data-responsive_offset="on"
+							data-frames='[{"delay":660,"speed":1280,"frame":"0","from":"y:50px;opacity:0;","to":"o:1;","ease":"Power3.easeInOut"},{"delay":"wait","speed":300,"frame":"999","to":"opacity:0;","ease":"Power3.easeInOut"}]'
+							data-textAlign="['inherit','inherit','inherit','inherit']" data-paddingtop="[0,0,0,0]"
+							data-paddingright="[0,0,0,0]" data-paddingbottom="[0,0,0,0]" data-paddingleft="[0,0,0,0]"
+							style="z-index: 11; white-space: nowrap; font-size: 16px; line-height: 22px; font-weight: 500; color: #333333; letter-spacing: 2px;font-family:Work Sans;">
+							FEATURED PRODUCT </div>
+						<div class="tp-caption   tp-resizeme" id="slide-14-layer-10" data-x="['left','left','left','left']"
+							data-hoffset="['281','91','88','21']" data-y="['top','top','top','top']"
+							data-voffset="['208','116','97','142']" data-fontsize="['48','48','48','40']"
+							data-lineheight="['64','64','64','45']" data-width="none" data-height="none"
+							data-whitespace="nowrap" data-type="text" data-responsive_offset="on"
+							data-frames='[{"delay":1050,"speed":1230,"frame":"0","from":"y:50px;opacity:0;","to":"o:1;","ease":"Power3.easeInOut"},{"delay":"wait","speed":300,"frame":"999","to":"opacity:0;","ease":"Power3.easeInOut"}]'
+							data-textAlign="['inherit','inherit','inherit','inherit']" data-paddingtop="[0,0,0,0]"
+							data-paddingright="[0,0,0,0]" data-paddingbottom="[0,0,0,0]" data-paddingleft="[0,0,0,0]"
+							style="z-index: 12; white-space: nowrap; font-size: 48px; line-height: 64px; font-weight: 400; color: #333333; letter-spacing: 0px;font-family:Work Sans;">
+							Montana shelf <br>
+							collection </div>
+
+						<!-- LAYER NR. 9 -->
+						<div class="tp-caption   tp-resizeme" id="slide-14-layer-11" data-x="['left','left','left','left']"
+							data-hoffset="['282','90','90','23']" data-y="['top','top','top','top']"
+							data-voffset="['368','283','262','250']" data-fontsize="['15','15','15','14']" data-width="none"
+							data-height="none" data-whitespace="nowrap" data-type="text" data-responsive_offset="on"
+							data-frames='[{"delay":1300,"speed":1180,"frame":"0","from":"y:50px;opacity:0;","to":"o:1;","ease":"Power3.easeInOut"},{"delay":"wait","speed":300,"frame":"999","to":"opacity:0;","ease":"Power3.easeInOut"}]'
+							data-textAlign="['inherit','inherit','inherit','inherit']" data-paddingtop="[0,0,0,0]"
+							data-paddingright="[0,0,0,0]" data-paddingbottom="[0,0,0,0]" data-paddingleft="[0,0,0,0]"
+							style="z-index: 13; white-space: nowrap; font-size: 15px; line-height: 26px; font-weight: 400; color: #7e7e7e; letter-spacing: 0.5px;font-family:Work Sans;">
+							Lorem ipsum dolor sit amet, consectetur cing elit. Suspe ndisse<br> suscipit sagittis leo estibulum
+							issim Lorem ipsum dolor sit amet,<br> consectetur cing elit. </div>
+
+						<!-- LAYER NR. 10 -->
+						<div class="tp-caption Slider-button-alt rev-btn " id="slide-14-layer-12"
+							data-x="['left','left','left','left']" data-hoffset="['282','91','89','29']"
+							data-y="['top','top','top','top']" data-voffset="['478','384','364','349']" data-width="none"
+							data-height="none" data-whitespace="nowrap" data-type="button" data-responsive_offset="on"
+							data-responsive="off"
+							data-frames='[{"delay":1580,"speed":1270,"frame":"0","from":"y:50px;opacity:0;","to":"o:1;","ease":"Power3.easeInOut"},{"delay":"wait","speed":300,"frame":"999","to":"opacity:0;","ease":"Power3.easeInOut"},{"frame":"hover","speed":"300","ease":"Linear.easeNone","to":"o:1;rX:0;rY:0;rZ:0;z:0;","style":"c:rgb(51,51,51);bg:transparent;"}]'
+							data-textAlign="['inherit','inherit','inherit','inherit']" data-paddingtop="[12,12,12,12]"
+							data-paddingright="[35,35,35,35]" data-paddingbottom="[12,12,12,12]"
+							data-paddingleft="[35,35,35,35]"
+							style="z-index: 14; white-space: nowrap; letter-spacing: 1px;border-color:rgba(0,0,0,1);outline:none;box-shadow:none;box-sizing:border-box;-moz-box-sizing:border-box;-webkit-box-sizing:border-box;cursor:pointer; width: 165px;">
+							<i class="fa fa-shopping-cart" aria-hidden="true"></i>
+							<a class="revslider-button" href="shop-left-sidebar.html"> ONLY $39</a> </div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="cosmetics-home-intro-area mb-70">
+		<div class="container" style="background-color: ;">
+
+			<div class="row align-items-center">
+				
+
+				<div class="col-md-6 pl-60 pl-sm-15 pl-xs-15 pl-xxs-15 mb-30 mt-30 text-center" style="background-color: ;">
+					
+					<!--=======  single image  =======-->
+					<img src="assets/images/mf/prod/pd_2_.png" class="img-fluid" alt="">
+					<!--=======  End of single image  =======-->
+				</div>
+				<div class="col-md-6 mb-30" style="background-color: #dccca7; height: 400px;">
+					<div class="cosmetics-home-intro mt-50">
+						<div class="tp-caption   tp-resizeme" id="slide-14-layer-9" data-x="['left','left','left','left']"
+							data-hoffset="['287','93','89','21']" data-y="['top','top','top','top']"
+							data-voffset="['170','73','58','108']" data-width="none" data-height="none" data-whitespace="nowrap"
+							data-type="text" data-responsive_offset="on"
+							data-frames='[{"delay":660,"speed":1280,"frame":"0","from":"y:50px;opacity:0;","to":"o:1;","ease":"Power3.easeInOut"},{"delay":"wait","speed":300,"frame":"999","to":"opacity:0;","ease":"Power3.easeInOut"}]'
+							data-textAlign="['inherit','inherit','inherit','inherit']" data-paddingtop="[0,0,0,0]"
+							data-paddingright="[0,0,0,0]" data-paddingbottom="[0,0,0,0]" data-paddingleft="[0,0,0,0]"
+							style="z-index: 11; white-space: nowrap; font-size: 16px; line-height: 22px; font-weight: 500; color: #333333; letter-spacing: 2px;font-family:Work Sans;">
+							FEATURED PRODUCT </div>
+						<div class="tp-caption   tp-resizeme" id="slide-14-layer-10" data-x="['left','left','left','left']"
+							data-hoffset="['281','91','88','21']" data-y="['top','top','top','top']"
+							data-voffset="['208','116','97','142']" data-fontsize="['48','48','48','40']"
+							data-lineheight="['64','64','64','45']" data-width="none" data-height="none"
+							data-whitespace="nowrap" data-type="text" data-responsive_offset="on"
+							data-frames='[{"delay":1050,"speed":1230,"frame":"0","from":"y:50px;opacity:0;","to":"o:1;","ease":"Power3.easeInOut"},{"delay":"wait","speed":300,"frame":"999","to":"opacity:0;","ease":"Power3.easeInOut"}]'
+							data-textAlign="['inherit','inherit','inherit','inherit']" data-paddingtop="[0,0,0,0]"
+							data-paddingright="[0,0,0,0]" data-paddingbottom="[0,0,0,0]" data-paddingleft="[0,0,0,0]"
+							style="z-index: 12; white-space: nowrap; font-size: 48px; line-height: 64px; font-weight: 400; color: #333333; letter-spacing: 0px;font-family:Work Sans;">
+							Montana shelf <br>
+							collection </div>
+
+						<!-- LAYER NR. 9 -->
+						<div class="tp-caption   tp-resizeme" id="slide-14-layer-11" data-x="['left','left','left','left']"
+							data-hoffset="['282','90','90','23']" data-y="['top','top','top','top']"
+							data-voffset="['368','283','262','250']" data-fontsize="['15','15','15','14']" data-width="none"
+							data-height="none" data-whitespace="nowrap" data-type="text" data-responsive_offset="on"
+							data-frames='[{"delay":1300,"speed":1180,"frame":"0","from":"y:50px;opacity:0;","to":"o:1;","ease":"Power3.easeInOut"},{"delay":"wait","speed":300,"frame":"999","to":"opacity:0;","ease":"Power3.easeInOut"}]'
+							data-textAlign="['inherit','inherit','inherit','inherit']" data-paddingtop="[0,0,0,0]"
+							data-paddingright="[0,0,0,0]" data-paddingbottom="[0,0,0,0]" data-paddingleft="[0,0,0,0]"
+							style="z-index: 13; white-space: nowrap; font-size: 15px; line-height: 26px; font-weight: 400; color: #7e7e7e; letter-spacing: 0.5px;font-family:Work Sans;">
+							Lorem ipsum dolor sit amet, consectetur cing elit. Suspe ndisse<br> suscipit sagittis leo estibulum
+							issim Lorem ipsum dolor sit amet,<br> consectetur cing elit. </div>
+
+						<!-- LAYER NR. 10 -->
+						<div class="tp-caption Slider-button-alt rev-btn " id="slide-14-layer-12"
+							data-x="['left','left','left','left']" data-hoffset="['282','91','89','29']"
+							data-y="['top','top','top','top']" data-voffset="['478','384','364','349']" data-width="none"
+							data-height="none" data-whitespace="nowrap" data-type="button" data-responsive_offset="on"
+							data-responsive="off"
+							data-frames='[{"delay":1580,"speed":1270,"frame":"0","from":"y:50px;opacity:0;","to":"o:1;","ease":"Power3.easeInOut"},{"delay":"wait","speed":300,"frame":"999","to":"opacity:0;","ease":"Power3.easeInOut"},{"frame":"hover","speed":"300","ease":"Linear.easeNone","to":"o:1;rX:0;rY:0;rZ:0;z:0;","style":"c:rgb(51,51,51);bg:transparent;"}]'
+							data-textAlign="['inherit','inherit','inherit','inherit']" data-paddingtop="[12,12,12,12]"
+							data-paddingright="[35,35,35,35]" data-paddingbottom="[12,12,12,12]"
+							data-paddingleft="[35,35,35,35]"
+							style="z-index: 14; white-space: nowrap; letter-spacing: 1px;border-color:rgba(0,0,0,1);outline:none;box-shadow:none;box-sizing:border-box;-moz-box-sizing:border-box;-webkit-box-sizing:border-box;cursor:pointer; width: 165px;">
+							<i class="fa fa-shopping-cart" aria-hidden="true"></i>
+							<a class="revslider-button" href="shop-left-sidebar.html"> ONLY $39</a> </div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="cosmetics-home-intro-area mb-70">
+		<div class="container" style="background-color: ;">
+
+			<div class="row align-items-center">
+				
+
+				<div class="col-md-6 pl-60 pl-sm-15 pl-xs-15 pl-xxs-15 mb-30 mt-30 text-center" style="background-color: ;">
+					
+					<!--=======  single image  =======-->
+					<img src="assets/images/mf/prod/pd_2_.png" class="img-fluid" alt="">
+					<!--=======  End of single image  =======-->
+				</div>
+				<div class="col-md-6 mb-30" style="background-color: #dccca7; height: 400px;">
+					<div class="cosmetics-home-intro mt-50">
 						<div class="tp-caption   tp-resizeme" id="slide-14-layer-9" data-x="['left','left','left','left']"
 							data-hoffset="['287','93','89','21']" data-y="['top','top','top','top']"
 							data-voffset="['170','73','58','108']" data-width="none" data-height="none" data-whitespace="nowrap"
