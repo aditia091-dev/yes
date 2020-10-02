ffd73167e82c401485450293aff459ca20919d44
aditia091-dev <azminadia2017@gmail.com>
2020-10-02 08:55:04 +0700

    fikri update loader page no delayed

--- application/views/landing/christianbreton.php
+++ application/views/landing/christianbreton.php
@@ -1,5 +1,4 @@
 <?php $this->load->view('third/style') ?>
 <?php $this->load->view('third/slide') ?>
-<?php $this->load->view('third/prod') ?>
 <?php $this->load->view('third/content') ?>
 <?php $this->load->view('third/footer') ?>
