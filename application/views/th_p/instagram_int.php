17833e2336f2809e26142a6dbd7fc01434c3d7ba
aditia091-dev <azminadia2017@gmail.com>
2020-10-07 13:16:33 +0700

    instagram add before footer

--- application/views/th_p/instagram_int.php
+++ application/views/th_p/instagram_int.php
@@ -11,7 +11,7 @@
           <div class="section-title section-title--one text-center">
             <h1>Follow us on Instagram</h1>
             <a href="https://instagram.com/chbreton_indonesia/"></a><p class="subtitle subtitle--deep mb-0" ><i class="fa fa-instagram" aria-hidden="true"></i>CHBRETON_INDONESIA</p>
-          </div></div></div></div>
+          </div></div></div></div></div>
 <br>
           <!--=======  End of section title  =======-->
 <!-- LightWidget WIDGET --><script src="https://cdn.lightwidget.com/widgets/lightwidget.js"></script><iframe src="//lightwidget.com/widgets/5d06cd7ff4745485b12f2a3b928ffad7.html" scrolling="no" allowtransparency="true" class="lightwidget-widget" style="width:100%;border:0;overflow:hidden;"></iframe>
